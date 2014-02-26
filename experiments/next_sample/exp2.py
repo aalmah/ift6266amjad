9019500f78cfe5ce96d38c543a4265dcf92dcec5
Amjad Almahairi <amjadmahayri@gmail.com>
2014-02-26 18:59:08 -0500

    removing and renaming files

--- experiments/next_sample/exp2.py
+++ experiments/next_sample/exp2.py
@@ -13,87 +13,13 @@ import numpy as np
 import theano
 import theano.tensor as T
 from theano import config
-from experiments.nn import NetworkLayer,MSE
+from experiments.nn import MLP, MSE, Rectifier
+from datasets_builder import build_data_sets
+from theano import config
 
 
-class MLPFramePredictor:
-    """Multi-Layer Perceptron for predicting the next acoustic samples frame
-    given previous samples and window of phonemes.
-    Inspired from Theano MLP tutorial http://deeplearning.net/tutorial/mlp.html
-    """
+def prepare_data(in_samples, out_samples, shift, win_width):
 
-    def __init__(self, input, rng, n_in, n_hiddens, h_activations,
-                 n_out, out_activation):
-        """Initialize the parameters for the multilayer perceptron
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type n_in: int
-        :param n_in: number of input units, the dimension of the space in
-        which the datapoints lie
-
-        :type n_hiddens: list of int
-        :param n_hiddens: a list of number of units in each hidden layer
-
-        :type activations: list of lambdas
-        :param n_hiddens: a list of activations used in each hidden layer
-        
-        :type n_out: int
-        :param n_out: number of output units, the dimension of the space in
-        which the labels lie
-
-        """
-
-        # We are dealing with multiple hidden layers MLP
-        layer1 = NetworkLayer(rng=rng, input=input,
-                              n_in=n_in, n_out=n_hiddens[0],
-                              activation=h_activations[0])
-        
-        h_layers = [('hiddenLayer1',layer1)]
-        
-        for i in range(1,n_hiddens[1:]):
-            h_layers['hiddenLayer%d' % (i+1)] = \
-                    NetworkLayer(rng=rng, input=h_layers[i-1][1].output,
-                                 n_in=n_hiddens[i-1], n_out=n_hiddens[i],
-                                 activation=h_activations[i])
-
-        self.__dict__update(dict(h_layers))
-        
-        # The output layer gets as input the hidden units
-        # of the hidden layer
-        self.outputLayer =  NetworkLayer(rng=rng,
-                                         input=h_layers[-1][1].output,
-                                         n_in=n_hiddens[-1],
-                                         n_out=n_out,
-                                         activation=out_activation)
-        
-        # L1 norm ; one regularization option is to enforce L1 norm to
-        # be small
-        self.L1 = abs(self.hiddenLayer.W).sum() \
-                + abs(self.outputLayer.W).sum()
-
-        # square of L2 norm ; one regularization option is to enforce
-        # square of L2 norm to be small
-        self.L2_sqr = (self.hiddenLayer.W ** 2).sum() \
-                    + (self.outputLayer.W ** 2).sum()
-
-        # the prediction is simply the output of the output layer
-        self.y_pred = self.outputLayer.output
-        
-        # the parameters of the model are the parameters of the all layers it
-        # is made out of
-        params = self.outputLayer.params
-        for layer in h_layers:
-            params.extend(layer.params)
-        self.params = params
-
-
-
-        
-def build_data_sets(frame_len):
-    """builds data sets for training/validating/testing the models"""
-    
     print 'loading data...'
 
     save_stdout = sys.stdout
@@ -102,175 +28,103 @@ def build_data_sets(frame_len):
     # creating wrapper object for TIMIT dataset
     dataset = TIMIT()
     dataset.load("train")
+    dataset.load("valid")
     
     sys.stdout = save_stdout
-
-    overlap = frame_len - 1
-
-    wav_seqs = dataset.train_raw_wav[0:10]
-    norm_seqs = utils.normalize(wav_seqs)
     
-    # Segment into frames
-    samples = map(lambda seq: segment_axis(seq, frame_len, overlap),
-                  norm_seqs)
-
-    # stack all data in one matrix, each row is a frame
-    data = np.vstack(samples)
-    # shuffle the frames so we can assume data is IID
-    np.random.seed(123)
-    data = np.random.permutation(data)
-
-    # take 10% for test, 10% for valid, and 80% for training
-    chunk = data.shape[0] / 10
-    # now split data to x and y for train, valid, and test
-    train_x = data[:8*chunk,:-1]
-    train_y = data[:8*chunk,-1]
-    valid_x = data[8*chunk:9*chunk,:-1]
-    valid_y = data[8*chunk:9*chunk,-1]
-    test_x = data[9*chunk:,:-1]
-    test_y = data[9*chunk:,-1]
-
-    print 'train_x shape', train_x.shape
-    print 'train_y shape', train_y.shape
-
-    print 'Done'
-    print 'There are %d training samples'%train_x.shape[0]
-    print 'There are %d validation samples'%valid_x.shape[0]
+    n_spkr = 10
+    n_utts = 10
+    shuffle = True
     
-    return utils.shared_dataset((train_x,train_y)),\
-           utils.shared_dataset((valid_x,valid_y)),\
-           utils.shared_dataset((test_x,test_y))
+    # each training example has 'in_sample' inputs and 'out_samples' output
+    # and examples are shifted by 'shift'
+    train_x, train_y1, train_y2 = \
+        build_data_sets(dataset, 'train', n_spkr, n_utts,
+                        in_samples, out_samples, shift,
+                        win_width,shuffle)
+
+    n_spkr = 1
+    n_utts = 10
+    shuffle = False
+    valid_x, valid_y1, valid_y2 = \
+        build_data_sets(dataset, 'valid', n_spkr, n_utts,
+                        in_samples, out_samples, shift,
+                        win_width,shuffle)
+    
+    return (train_x, train_y1, train_y2), (valid_x, valid_y1, valid_y2)
 
         
+def train_model(datasets, learning_rate=0.01, L2_reg=0.0001,
+                n_epochs=10, batch_size=100,
+                in_samples = 240, out_samples = 50, shift=10, win_width=2,
+                output_folder='output_folder'):
         
-def train_test_model(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001,
-                     n_epochs=10, batch_size=100, frame_len=15*16,
-                     n_hidden=200,
-                     output_folder='output_folder'):
-    """
-    Trains the next acoustic sample predictor using SGD
-    
-    :type learning_rate: float
-    :param learning_rate: learning rate used (factor for the stochastic
-    gradient
+    train_x, train_y1, train_y2 = datasets[0]
+    valid_x, valid_y1, valid_y2 = datasets[1]
 
-    :type L1_reg: float
-    :param L1_reg: L1-norm's weight when added to the cost (see
-    regularization)
-    
-    :type L2_reg: float
-    :param L2_reg: L2-norm's weight when added to the cost (see
-    regularization)
-    
-    :type n_epochs: int
-    :param n_epochs: maximal number of epochs to run the optimizer
 
-    :type batch_size
-    :param batch_size: number of training examples in each minibatch
-
-    """
-    
-    
-    rng = np.random.RandomState(1234)
-    index = T.iscalar()  # index to a [mini]batch
-    x = T.fmatrix('x')   # input data
-    y = T.fvector('y')   # the target values
-    
-    datasets = build_data_sets(frame_len)
-        
-    train_set_x, train_set_y = datasets[0]
-    valid_set_x, valid_set_y = datasets[1]
-
-    # compute number of minibatches for training, validation and testing
-    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
-    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] / batch_size
-
-    
     ######################
     # BUILD ACTUAL MODEL #
     ######################
     print 'building the model...'
 
+    rng = np.random.RandomState(1234)
+
+    x = T.fmatrix('x')   # input data
+    y1 = T.fmatrix('y1')   # the target frames
+    # y2 = T.fvector('y2')  # the target shift
+
+    #debugging
+    #import pdb; pdb.set_trace()
+    
+    # config.compute_test_value = 'warn'
+    
+    # x.tag.test_value = train_x.get_value()[:200]
+    # y1.tag.test_value = train_y1.get_value()[:200]    
+
+    
+    rectifier = lambda x: Rectifier(x)
+    #sigmoid = theano.tensor.nnet.sigmoid
+    
      # construct model class
-    model = NextSamplePredictor(rng=rng, input=x,
-                            n_in=frame_len-1,
-                            n_hidden=n_hidden,
-                            n_out=1)
+    frame_pred = MLP(rng=rng, input=x,
+                     n_in=in_samples+win_width*39,
+                     n_hiddens=[300,300],
+                     hid_activations=[T.tanh, T.tanh],
+                     # n_hiddens=[100],
+                     # hid_activations=[],
+                     n_out=out_samples,
+                     out_activation=T.tanh)
+
+    # shift_pred = MLP(rng=rng, input=x,
+    #                  n_in=in_samples,
+    #                  n_hiddens=[500],
+    #                  hid_activations=[T.nnet.sigmoid],
+    #                  n_out=1,
+    #                  out_activation=T.nnet.sigmoid)
     
     
-    cost = MSE(y_pred=model.y_pred, y=y) \
-           + L1_reg * model.L1 \
-           + L2_reg * model.L2_sqr
+    frame_cost = MSE(y_pred=frame_pred.y_pred, y=y1) \
+                 + L2_reg * frame_pred.L2_sqr
 
 
-    validate_model = theano.function(inputs=[index],
-            outputs=MSE(y_pred=model.y_pred, y=y),
-            givens={
-                x: valid_set_x[index * batch_size:(index + 1) * batch_size],
-                y: valid_set_y[index * batch_size:(index + 1) * batch_size]})
-
-    gparams = []
-    for param in model.params:
-        gparam = T.grad(cost, param)
-        gparams.append(gparam)
-
-    updates = []
-
-    for param, gparam in zip(model.params, gparams):
-        updates.append((param, param - learning_rate * gparam))
-
-
-    train_model = theano.function(inputs=[index], outputs=cost,
-            updates=updates,
-            givens={
-                x: train_set_x[index * batch_size:(index + 1) * batch_size],
-                y: train_set_y[index * batch_size:(index + 1) * batch_size]})
+    total_training_costs, total_validation_costs = \
+        frame_pred.train(y=y1, training_loss=frame_cost,
+                         learning_rate=learning_rate, n_epochs=n_epochs,\
+                         train_x=train_x, train_y=train_y1, valid_x=valid_x,\
+                         valid_y=valid_y1, batch_size=batch_size)
     
-                    
-    ###############
-    # TRAIN MODEL #
-    ###############
-    print 'training...'
-
-    if not os.path.isdir(output_folder):
-        os.makedirs(output_folder)
-
-    start_time = time.clock()
-    epoch = 0
-
-    total_training_losses = []
-    total_validation_losses = []
     
-    while (epoch < n_epochs):
-        epoch = epoch + 1
-        training_losses = []
-        for minibatch_index in xrange(n_train_batches):
-
-            minibatch_avg_cost = train_model(minibatch_index)
-            training_losses.append(minibatch_avg_cost)
-                
-        
-        this_training_loss = np.mean(training_losses)
-        validation_losses = [validate_model(i) for i
-                             in xrange(n_valid_batches)]
-        this_validation_loss = np.mean(validation_losses)
-            
-        total_training_losses.append(this_training_loss)
-        total_validation_losses.append(this_validation_loss)
-            
-        print 'epoch %i, training MSE %f, validation MSE %f' %\
-        (epoch, this_training_loss,this_validation_loss)
-
-    end_time = time.clock()
-
-    print "Training took %.2f minutes..."%((end_time-start_time)/60.)
-
-    model.save_model(output_folder=output_folder)
+    frame_pred.save_model(output_folder=output_folder)
 
     #save losses and parameters..
     
-    np.save(os.path.join(output_folder,'training_MSE'), np.asarray(total_training_losses))
-    np.save(os.path.join(output_folder,'validation_MSE'), np.asarray(total_validation_losses))
+    np.save(os.path.join(output_folder,'training_MSE'),
+            np.asarray(total_training_costs))
+    np.save(os.path.join(output_folder,'validation_MSE'),
+            np.asarray(total_validation_costs))
+
+    
     
 if __name__ == "__main__":
     
@@ -278,18 +132,40 @@ if __name__ == "__main__":
     # FRAME_LEN_MS = 15
     
     # frame_len = FRAME_LEN_MS * SAMPLE_PER_MS
-    # build_data_sets(frame_len)
+    #build_data_sets(frame_len)
     # train_test_model(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001,
     #                  n_epochs=100, batch_size=1000, frame_len=frame_len,
     #                  n_hidden=500, output_folder='test_output')
 
-    rng = np.random.RandomState(1234)
-    x = T.fmatrix('x')   # input data
+    # win_width = 2
+    # i = 0
+    # for in_s in np.arange(2,6)*120:
+    #     for out_s in np.arange(1,3)*40:
+    #         for sh in np.arange(1,3)*20:
+    #             i = i + 1
+    #             # if i == 1:
+    #             #     datasets = prepare_data(in_s, out_s, sh, win_width)
+    #             #     np.save('out_%d/datasets'%i, datasets)
+                    
+    #             #os.mkdir('out_%d'%i)
+    #             # train_model(datasets, learning_rate=0.01, L2_reg=0.0001,
+    #             #             n_epochs=50, batch_size=100,
+    #             #             in_samples = in_s, out_samples = out_s,
+    #             #             shift = sh, output_folder='out_%d'%i)
+    #             # best: 1 5 9 13
+    #             val = np.load('out_%d/validation_MSE.npy'%i)[-1]
+    #             tra = np.load('out_%d/training_MSE.npy'%i)[-1]
+    #             print '%d: in_size: %d, out_size: %d, shift: %d, valid: %f, train: %f'%(i,in_s, out_s, sh, val, tra)
+
+    # picking 13 :
+    in_s = 600
+    out_s = 40
+    sh = 20
+    win_width = 2
     
-    frame_pred = MLPFramePredictor(rng=rng, input=x,
-                                   n_in=240,
-                                   n_hiddens=[300,300,300],
-                                   h_activations=[T.]
-                                   n_out=1)
-    
-    
+    #os.mkdir('better_results')
+    datasets = prepare_data(in_s, out_s, sh, win_width)
+    train_model(datasets, learning_rate=0.01, L2_reg=0.0001,
+                n_epochs=100, batch_size=100,
+                in_samples = in_s, out_samples = out_s,
+                shift = sh, output_folder='better_results')
